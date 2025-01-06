28c3ae21d71b2d88027655bc9559fcd8a3ad0b6a
Robert Stolz <robert.stolz87@gmail.com>
2025-01-06 20:13:41 +0100

    1st test

--- backend/src/test/java/ropold/backend/controller/LessonControllerIntegrationTests.java
+++ backend/src/test/java/ropold/backend/controller/LessonControllerIntegrationTests.java
@@ -1,4 +1,69 @@
 package ropold.backend.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ropold.backend.model.Category;
+import ropold.backend.model.LessonModel;
+import ropold.backend.repository.LessonRepository;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class LessonControllerIntegrationTests {
+
+    static LessonModel lessonModel;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    LessonRepository lessonRepository;
+
+    @Autowired
+    void setup() {
+        lessonRepository.deleteAll();
+
+        lessonModel = new LessonModel(
+                "1",
+                true,
+                1,
+                "Testlesson",
+                "test description",
+                Category.BEGINNER,
+                "testImageUrl"
+        );
+
+        lessonRepository.save(lessonModel);
+    }
+
+    @Test
+    void getAllLessons_expectListWithOneLesson_whenOneLessonSaved() throws Exception {
+        // WHEN
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/lesson")
+                )
+                // THEN
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                [
+                    {
+                        "id": "1",
+                        "isActive": true,
+                        "count": 1,
+                        "title": "Testlesson",
+                        "description": "test description",
+                        "category": "BEGINNER",
+                        "imageUrl": "testImageUrl"
+                    }
+                ]
+            """));
+    }
+
+
 }
