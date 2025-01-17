3f059225207fe4571df33e04bf0fc604b2542f0d
Robert Stolz <robert.stolz87@gmail.com>
2025-01-17 21:07:18 +0100

    nav

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,15 @@
 
 export default function Navbar() {
     return (
-        <h2>Navbar</h2>
+        <div className="navbar">
+            <div className="clickable-header">
+                <button>Home</button>
+                <button>search</button>
+                <button>login with Github</button>
+                <button>logout</button>
+            </div>
+
+        </div>
+
     )
 }
