b79e79454fb2d0197c30f5bcf25c447113c8ce31
Robert Stolz <robert.stolz87@gmail.com>
2025-01-20 16:16:13 +0100

    UserIntegraiontest

--- backend/src/test/java/ropold/backend/security/UserIntegrationTest.java
+++ backend/src/test/java/ropold/backend/security/UserIntegrationTest.java
@@ -1,16 +1,36 @@
 package ropold.backend.security;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserIntegrationTest {
+class UserIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
 
+    @Test
+    @WithMockUser(username = "user")
+    void testGetMe_withLoggedInUser_expectUsername() throws Exception {
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+    }
+
+    @Test
+    void testGetMe_withoutLoggedInUser_expectAnonymousUser() throws Exception {
+        mockMvc.perform(get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("anonymousUser"));
+    }
 
 }
