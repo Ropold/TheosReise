7ba69bf3f925feac1a734071045879d504de882c
Robert Stolz <robert.stolz87@gmail.com>
2025-01-21 19:22:45 +0100

    cloudinary service start 2nd

--- backend/src/main/java/ropold/backend/controller/LessonController.java
+++ backend/src/main/java/ropold/backend/controller/LessonController.java
@@ -1,12 +1,18 @@
 package ropold.backend.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ropold.backend.model.LessonModel;
 import ropold.backend.model.LessonModelDto;
+import ropold.backend.service.CloudinaryService;
 import ropold.backend.service.LessonService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,6 +21,7 @@ import java.util.List;
 public class LessonController {
 
     private final LessonService lessonService;
+    private final CloudinaryService cloudinaryService;
 
     @GetMapping()
     public List<LessonModel> getAllLessons() {
@@ -26,9 +33,33 @@ public class LessonController {
         return lessonService.getLessonById(id);
     }
 
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping
+//    public LessonModel addLesson(@RequestBody LessonModelDto lessonModelDto) {
+//        return lessonService.addLesson(
+//                new LessonModel(
+//                        null,
+//                        true,
+//                        lessonModelDto.count(),
+//                        lessonModelDto.title(),
+//                        lessonModelDto.description(),
+//                        lessonModelDto.category(),
+//                        lessonModelDto.imageUrl()
+//                )
+//        );
+//    }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public LessonModel addLesson(@RequestBody LessonModelDto lessonModelDto) {
+    public LessonModel addLesson(
+            @RequestPart("lessonModelDto") @Valid LessonModelDto lessonModelDto,
+            @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
+
+        String imageUrl = null;
+        if (image != null && !image.isEmpty()) {
+            imageUrl = cloudinaryService.uploadImage(image);
+        }
+
         return lessonService.addLesson(
                 new LessonModel(
                         null,
@@ -37,7 +68,7 @@ public class LessonController {
                         lessonModelDto.title(),
                         lessonModelDto.description(),
                         lessonModelDto.category(),
-                        lessonModelDto.imageUrl()
+                        imageUrl
                 )
         );
     }
--- backend/src/main/java/ropold/backend/service/CloudinaryService.java
+++ backend/src/main/java/ropold/backend/service/CloudinaryService.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +16,11 @@ public class CloudinaryService  {
 
     private final Cloudinary cloudinary;
 
-    public void uploadImage(MultipartFile image) throws IOException {
-        cloudinary.uploader().upload(image, Collections.emptyMap());
+    public String uploadImage(MultipartFile image) throws IOException {
+        File fileToUpload = File.createTempFile("TheosReise", "");
+        image.transferTo(fileToUpload);
+
+        Map uploadResult = cloudinary.uploader().upload(fileToUpload, Collections.emptyMap());
+        return uploadResult.get("secure_url").toString();
     }
 }
