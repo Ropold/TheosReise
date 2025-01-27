998e5956180886b157cbb142b2cd74d0b4216811
Robert Stolz <robert.stolz87@gmail.com>
2025-01-27 10:29:20 +0100

    soundfile

--- frontend/src/components/lesson-content/Lesson1.tsx
+++ frontend/src/components/lesson-content/Lesson1.tsx
@@ -1,9 +1,15 @@
-
-export default function Lesson1(){
+export default function Lesson1() {
     return (
         <div className="lesson-content-card-container">
-            <h3>Einführung in die Musiktheorie - Lesson1.tsx:</h3>
-            <p>Lorem Ipsum 1</p>
+            <h3>Kapitel 1 Oma Elfi - Lesson1.tsx:</h3>
+                        {/* Audio Player */}
+            <audio controls>
+                <source
+                    src="https://res.cloudinary.com/dzjjlydk3/video/upload/v1737969925/Kapitel_1_id96p7.m4a"
+                    type="audio/mpeg"
+                />
+                Dein Browser unterstützt das Audioelement nicht.
+            </audio>
         </div>
-    )
+    );
 }
